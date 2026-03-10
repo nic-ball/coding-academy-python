3029635e527f3e69f1ae4eb8a7d2f3a4fe3d9c3c
nic-ball <nicholasball147@gmail.com>
2026-03-10 14:33:12 +0000

    More examples

--- guis/menu_bar.py
+++ guis/menu_bar.py
@@ -8,6 +8,19 @@ def my_handler():
 root = Tk()
 root.title('Simple Menu Demo')
 
+# Function to open a new window
+def open_new_window():
+    new_window = Toplevel(root)
+    new_window.title('New Window')
+    new_window.geometry('500x300')
+
+    lbl = Label(new_window, text='This is a NEW Window....', font=('monospace', 14))
+    lbl.pack(pady=20)
+
+    text2 = Text(new_window)
+    text2.pack(fill=BOTH, expand=True)
+    text2.insert(1.0, 'Text inside the new window...')
+
 text1 = Text(root)
 text1.pack(fill=BOTH, expand=True)
 
@@ -18,7 +31,7 @@ menubar = Menu(root)
 file_menu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label='New', command=my_handler)
-file_menu.add_command(label='Open', command=None)
+file_menu.add_command(label='Open', command=open_new_window) # Opens new window
 file_menu.add_command(label='Save', command=None)
 file_menu.add_separator()
 file_menu.add_command(label='Exit', command=root.quit)
